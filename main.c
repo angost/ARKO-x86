bc325917d791fe675df4ed39d05c036cfa92aed8
Angelika Ostrowska <01178572@pw.edu.pl>
2023-05-29 13:06:30 +0200

    Reorganised main

--- main.c
+++ main.c
@@ -5,6 +5,34 @@
 // arg: wskaznik na tablice pikseli 1 i 2, wymiary obrazkow, wybrany pkt odniesienia
 void f(uint8_t *pixels_picture_under, uint8_t *pixels_picture_above, uint16_t width, uint16_t height, uint16_t x, uint16_t y, float alpha_temporary);
 
+
+void setup_picture_above(uint32_t pixel_array_len, uint8_t *pixels_picture_above){
+    for (int i = 0; i < pixel_array_len/2; i++) {
+        int remainder = i % 3;
+        if (remainder == 0)
+            pixels_picture_above[i] = 255;
+        else if (remainder == 1)
+            pixels_picture_above[i] = 0;
+        else
+            pixels_picture_above[i] = 150;
+    }
+
+    for (int i = pixel_array_len/2; i < pixel_array_len; i++) {
+        int remainder = i % 3;
+        if (remainder == 0)
+            pixels_picture_above[i] = 252;
+        else if (remainder == 1)
+            pixels_picture_above[i] = 168;
+        else
+            pixels_picture_above[i] = 3;
+    }
+}
+
+void get_input(uint16_t *input_coords){
+    input_coords[0] = 2; //x
+    input_coords[1] = 3; //y
+}
+
 int main(int argc, char *argv[])
 {
     // SETUP
@@ -16,17 +44,8 @@ int main(int argc, char *argv[])
     uint8_t pixels_picture_under[pixel_array_len];
     uint8_t pixels_picture_above[pixel_array_len];
     uint8_t pixels_result_picture[pixel_array_len];
-    //roboczo
-    pixels_picture_under[(2*width+4)*3] = 40;
-    for (int i = 0; i < pixel_array_len; i++) {
-        int remainder = i % 3;
-        if (remainder == 0)
-            pixels_picture_above[i] = 255;
-        else if (remainder == 1)
-            pixels_picture_above[i] = 0;
-        else
-            pixels_picture_above[i] = 150;
-    }
+
+    setup_picture_above(pixel_array_len, pixels_picture_above);
 
     ALLEGRO_DISPLAY *display = NULL;
     if (!al_init())
@@ -34,40 +53,44 @@ int main(int argc, char *argv[])
     display = al_create_display(width, height);
     al_init_primitives_addon();
 
-    // INPUT: WYBIERZ PUNKT ODNIESIENIA
-    uint16_t x_input = 2;
-    uint16_t y_input = 3;
+    uint16_t input_coords[2];
+
+
+    // skopiuj poczatkowy obrazek - ROBOCZO
     for (int i = 0; i < pixel_array_len; i++){
         pixels_result_picture[i] = pixels_picture_above[i];
     }
-    // ASSEMBLER, PRZETWARZANIE DANYCH
-    //printf("%d, %d, %d\n", pixels_result_picture[639*3], pixels_result_picture[639*3+1], pixels_result_picture[639*3+2]);
-    printf("%d, %d, %d\n", pixels_result_picture[600], pixels_result_picture[601], pixels_result_picture[602]);
-    float alpha_temporary = 0.75;
-    f(pixels_picture_under, pixels_result_picture, width, height, x_input, y_input, alpha_temporary);
-    // printf("%d, %d, %d\n", pixels_result_picture[639*3], pixels_result_picture[639*3+1], pixels_result_picture[639*3+2]);
-    printf("%d, %d, %d\n", pixels_result_picture[600], pixels_result_picture[601], pixels_result_picture[602]);
-    // WYSWIETLANIE
-    // Konwencja w Allegro : 00 to lewy gorny
-    for (int row = 0; row < height; row++){
-        for (int column = 0; column < width; column++){
-            uint32_t r_index = (row*width + column)*3;
-            uint32_t g_index = r_index + 1;
-            uint32_t b_index = r_index + 2;
-            al_draw_pixel(column, row, al_map_rgb(pixels_result_picture[r_index], pixels_result_picture[g_index], pixels_result_picture[b_index]));
+
+    int max_loop_nr = 2;
+    for (int loop_nr = 0; loop_nr < max_loop_nr; loop_nr++) {
+        // WYSWIETLANIE
+        // Konwencja w Allegro : 00 to lewy gorny
+        for (int row = 0; row < height; row++){
+            for (int column = 0; column < width; column++){
+                uint32_t r_index = (row*width + column)*3;
+                uint32_t g_index = r_index + 1;
+                uint32_t b_index = r_index + 2;
+                al_draw_pixel(column, row, al_map_rgb(pixels_result_picture[r_index], pixels_result_picture[g_index], pixels_result_picture[b_index]));
+            }
         }
+        al_flip_display();
+        al_rest(5.0);
+
+        // INPUT: WYBIERZ PUNKT ODNIESIENIA
+        get_input(input_coords);
+        float alpha_temporary = 0.5;
+
+        // skopiuj poczatkowy obrazek
+        for (int i = 0; i < pixel_array_len; i++){
+            pixels_result_picture[i] = pixels_picture_above[i];
+        }
+        // ASSEMBLER, PRZETWORZ
+        f(pixels_picture_under, pixels_result_picture, width, height, input_coords[0], input_coords[1], alpha_temporary);
     }
-    al_flip_display();
-    al_rest(5.0);
+
     al_destroy_display(display);
-
     return 0;
-
-// ALLEGRO_COLOR get_rgb(uint8_t *pixel_array, row, column)
-//     ALLEGRO_COLOR white = al_map_rgb_f(1, 1, 1);
-
-
-
+}
 
     // initDefaultParams(&x, &y);
     // uint8_t pPixelArray = allocPixelArray(WIDTH, HEIGHT);
@@ -77,6 +100,3 @@ int main(int argc, char *argv[])
     //     waitForUserInput(?);
     //     modifyParamsAccordingToUserInput(&x, &y, &z, ?);
     // }
-
-}
-
